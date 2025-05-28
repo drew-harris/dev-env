13ef8f774f4079ffa01884c4bf7321c2843f1af4
Drew Harris <drew@drewh.net>
2025-05-27 22:23:54 -0500

    fix formatting

--- config/nvim/lua/user/lsp/formatting.lua
+++ config/nvim/lua/user/lsp/formatting.lua
@@ -17,13 +17,12 @@ require("conform").setup({
 		-- python = { "isort", "black" },
 		go = { "goimports", "gofmt" },
 		-- Use a sub-list to run only the first available formatter
-		javascript = { "prettierd" },
-		javascriptreact = { "prettierd" },
-		typescript = { "prettierd" },
-		typescriptreact = { "prettierd" },
+		javascript = { "biome", "prettierd"  },
+		javascriptreact = { "biome", "prettierd" },
+		typescript = { "biome", "prettierd" },
+		typescriptreact = { "biome", "prettierd" },
 		astro = { "prettierd" },
 		java = { "google-java-format" },
-		css = { "" },
 	},
 	format_on_save = function(bufnr)
 		if not do_autoformat then
