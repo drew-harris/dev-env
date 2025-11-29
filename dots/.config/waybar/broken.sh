d5702ef6f7e15b0b6b8cf8de629b94114275b114
Drew Harris <drew@drewh.net>
2025-11-29 09:33:01 -0800

    updates!!!!

--- dots/.config/waybar/broken.sh
+++ dots/.config/waybar/broken.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-# 
+
 print "hi";
 print "Change detected in $path. Reloading Waybar..."
 pkill -x waybar
@@ -7,4 +7,3 @@ CONFIG_FILE="$(pwd)/config.jsonc"
 STYLE_FILE="$(pwd)/style.css"
 waybar -c $CONFIG_FILE -s $STYLE_FILE &
 print "Waybar reloaded."
-
